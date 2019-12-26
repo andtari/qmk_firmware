aaa5d4a89eb1036e054647fddcc321d0b64d89c1
andtari <andtar@gmail.com>
2019-12-26 13:20:29 +0100

    KBD75 iso-sv cleanup

--- keyboards/kbdfans/kbd75/keymaps/iso-sv/keymap.c
+++ keyboards/kbdfans/kbd75/keymaps/iso-sv/keymap.c
@@ -1,23 +1,21 @@
 #include QMK_KEYBOARD_H
 #include "keymap_swedish.h"
 
-// Macros
-// name macros here for keymap reference
-enum {
-  EMAIL_ADD = SAFE_RANGE,
-  OTHER_MACRO
-};
+#define _BL 0
+#define _FL 1
 
+enum {
+  M_EMAIL = SAFE_RANGE,
+  M_OTHER
+};
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   if(record->event.pressed){
     switch(keycode) {
-      // copy these lines to define new macro
-      case EMAIL_ADD:
+      case M_EMAIL:
         SEND_STRING("youremail"SS_RALT("2")"youremail.com");
         return false; break;
-      // copy to here
-      case OTHER_MACRO:
+      case M_OTHER:
         SEND_STRING("The Other Macro");
         return false; break;
     }
@@ -43,7 +41,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * `----------------------------------------------------------------'
    */
 
-	[0] = LAYOUT_iso_1u(
+	[_BL] = LAYOUT_iso_1u(
     KC_ESC,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,   KC_PSCR,  KC_PAUS,  KC_DEL,
     KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,  KC_EQL,             KC_BSPC,  KC_HOME,
     KC_TAB,             KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,  KC_ENT,   KC_END,
@@ -68,12 +66,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * `----------------------------------------------------------------'
    */
 
-	[1] = LAYOUT_iso_1u(
+	[_FL] = LAYOUT_iso_1u(
     _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,
     _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,            RESET,    _______,
-    _______,            _______,  _______,  EMAIL_ADD,_______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,
+    _______,            _______,  _______,  M_EMAIL,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,
     _______,            RGB_TOG,  RGB_MOD,  RGB_HUI,  RGB_HUD,  RGB_SAI,  RGB_SAD,  RGB_VAI,  RGB_VAD,  _______,  _______,  _______,  _______,            _______,
-    _______,  _______,  _______,  _______,  BL_DEC,   BL_TOGG,  BL_INC,   BL_STEP,  _______,  KC_VOLD,  KC_VOLU,  KC_MUTE,            KC_BTN1, KC_MS_U, KC_BTN2,
-    _______,  _______,  _______,                      _______,                                          _______,  _______,  _______,  KC_MS_L, KC_MS_D, KC_MS_R
+    _______,  _______,  _______,  _______,  BL_DEC,   BL_TOGG,  BL_INC,   BL_STEP,  _______,  KC_VOLD,  KC_VOLU,  KC_MUTE,            KC_BTN1,  KC_MS_U,  KC_BTN2,
+    _______,  _______,  _______,                      _______,                                          _______,  _______,  _______,  KC_MS_L,  KC_MS_D,  KC_MS_R
   )
 };
