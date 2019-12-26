3b7666b23839a3b64572f86dfaefc336eec416f3
andtari <andtar@gmail.com>
2019-12-26 13:22:24 +0100

    TADA68 iso-sv cleanup

--- keyboards/tada68/keymaps/iso-sv/keymap.c
+++ keyboards/tada68/keymaps/iso-sv/keymap.c
@@ -4,23 +4,18 @@
 #define _BL 0
 #define _FL 1
 
-// Macros
-// name macros here for keymap reference
 enum {
-  EMAIL_ADD = SAFE_RANGE,
-  OTHER_MACRO
+  M_EMAIL = SAFE_RANGE,
+  M_OTHER
 };
-
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   if(record->event.pressed){
     switch(keycode) {
-      // copy these lines to define new macro
-      case EMAIL_ADD:
+      case M_EMAIL:
         SEND_STRING("youremail"SS_RALT("2")"youremail.com");
         return false; break;
-      // copy to here
-      case OTHER_MACRO:
+      case M_OTHER:
         SEND_STRING("The Other Macro");
         return false; break;
     }
@@ -29,6 +24,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+
   /* Keymap _BL: (Base Layer) Default Layer
    * ,----------------------------------------------------------------.
    * |Esc | 1|  2|  3|  4|  5|  6|  7|  8|  9|  0|  +|  ´|Backspa |Hme|
@@ -42,12 +38,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |Ctrl|Alt |Cmd |        Space          |Alt| FN|Ctrl|Lef|Dow|Rig |
    * `----------------------------------------------------------------'
    */
+  
   [_BL] = LAYOUT_iso(
-    KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC, KC_PSCR,  \
-    KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC,          KC_DEL,  \
-    KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_BSLS, KC_ENT,  KC_PGUP, \
-    KC_LSFT, KC_NUBS, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, KC_UP,   KC_PGDN, \
-    KC_LCTL, KC_LGUI, KC_LALT,                   KC_SPC,                             KC_RALT, MO(_FL), KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT
+    KC_ESC,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,  KC_EQL,   KC_BSPC,  KC_PSCR,
+    KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,            KC_DEL,
+    KC_LCTL,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,  KC_BSLS,  KC_ENT,   KC_PGUP,
+    KC_LSFT,  KC_NUBS,  KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  KC_RSFT,  KC_UP,    KC_PGDN,
+    KC_LCTL,  KC_LGUI,  KC_LALT,                      KC_SPC,                                 KC_RALT,  MO(_FL),  KC_RCTL,  KC_LEFT,  KC_DOWN,  KC_RGHT
   ),
 
   /* Keymap _FL: Function Layer
@@ -67,12 +64,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * Mouse: KC_BTN1, KC_BTN2, KC_MS_L, KC_MS_R, KC_MS_U, KC_MS_D
    * 
    */
-[_FL] = LAYOUT_ansi(
-  _______,  KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,   RESET,    KC_PSCR, \
-  _______,  _______,  KC_UP,    EMAIL_ADD,_______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  KC_INS, \
-  _______,  KC_LEFT,  KC_DOWN,  KC_RIGHT, _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,            _______,  KC_HOME, \
-  _______,  _______,  _______,  BL_DEC,   BL_TOGG,  BL_INC,   _______,  KC_VOLD,  KC_VOLU,  KC_MUTE,  _______,  KC_BTN1,  KC_MS_U,  KC_BTN2, \
-  _______,  _______,  _______,                      _______,                                _______,  _______,  _______,  KC_MS_L,  KC_MS_D, KC_MS_R
-  ),
+
+  [_FL] = LAYOUT_iso(
+    _______,  KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,   RESET,    KC_PSCR,
+    _______,  _______,  KC_UP,    M_EMAIL,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,            KC_INS,
+    _______,  KC_LEFT,  KC_DOWN,  KC_RIGHT, _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  KC_HOME,
+    _______,  _______,  _______,  BL_DEC,   BL_TOGG,  BL_INC,   _______,  KC_VOLD,  KC_VOLU,  KC_MUTE,  _______,  _______,  KC_BTN1,  KC_MS_U,  KC_BTN2,
+    _______,  _______,  _______,                      _______,                                _______,  _______,  _______,  KC_MS_L,  KC_MS_D,  KC_MS_R
+    ),
 };
 
